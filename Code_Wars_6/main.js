d2c82f16046f06db77ed7727352ac6629919d6ec
Matthew Cummings <cummingsmatthew97@gmail.com>
2022-04-30 10:28:26 -0400

    new challenge

---
+++ Code_Wars_6/main.js
@@ -0,0 +1,11 @@
+function peopleWithAgeDrink(old) { 
+    if(old > 20){
+      return 'drink whisky'
+    }else if(old > 18){
+      return 'drink beer'
+    }else if(old > 14){
+      return 'drink coke'
+    }else{
+      return 'drink toddy'
+    }
+  };
