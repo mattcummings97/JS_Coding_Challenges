27f3327675c82052ffd1acfe4fc8cd0f2fac15c5
Matthew Cummings <cummingsmatthew97@gmail.com>
2022-05-01 09:07:34 -0400

    new challenge

---
+++ Code_Wars_7/main.js
@@ -0,0 +1,11 @@
+function isTriangle(a,b,c) {
+    if(a > 0 && b > 0 && c > 0) {
+      if(a < b + c && b < a + c && c < a + b) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+  }
+  
