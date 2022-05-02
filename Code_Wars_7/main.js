69954cf10a7f3ef9c82484ced8c5d36317b3ee2e
Matthew Cummings <cummingsmatthew97@gmail.com>
2022-05-02 18:16:11 -0400

    new challenge

--- Code_Wars_7/main.js
+++ Code_Wars_7/main.js
@@ -1,3 +1,5 @@
+/*Triangle Problem */ 
+
 function isTriangle(a,b,c) {
     if(a > 0 && b > 0 && c > 0) {
       if(a < b + c && b < a + c && c < a + b) {
---
+++ Code_Wars_8/main.js
@@ -0,0 +1,6 @@
+/* In this simple assignment you are given a number and have to make it negative. */
+function makeNegative(num) {
+    return -Math.abs(num)
+  }
+
+makeNegative()
