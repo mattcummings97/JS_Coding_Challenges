8654b2f7eb78d3a9b5c1627021f5268372e06662
Matthew Cummings <cummingsmatthew97@gmail.com>
2022-05-19 14:57:22 -0400

    new challenges

---
+++ Code_Wars_23/main.js
@@ -0,0 +1,6 @@
+/* Write a function which converts the input string to uppercase. */ 
+
+function makeUpperCase(str) {
+    let upString = str.toUpperCase('')
+    return upString
+  }
--- code_wars_practice_area/main.js
+++ code_wars_practice_area/main.js
@@ -42,8 +42,21 @@ let revX = x.split('').reverse('').join('')
 
 x(cow) */ 
 
-function highAndLow(numbers){
+/*function highAndLow(numbers){
     numbers = '1 2 3 4 5'
     splitNum = Number(numbers.split(''))
     console.log(splitNum)
+
+  }*/ 
+
+
+  function findNeedle(haystack) {
+    for(let i=0;i<haystack.length;i++){
+      if(haystack[i]=== 'needle'){
+        console.log( `found the needle at position {haystack[i]}`)
+      }else{
+        console.log(false)
+      }
+    }
   }
+  findNeedle(['needle','2',2, true])
