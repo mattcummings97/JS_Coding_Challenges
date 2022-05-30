6aa1592928c25704a81671dcc12ec9cbfbbe06d4
Matthew Cummings <cummingsmatthew97@gmail.com>
2022-05-30 13:31:04 -0400

    new challenge

---
+++ Code_Wars_31/main.js
@@ -0,0 +1,19 @@
+/* Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter. */ 
+
+const quarterOf = (month) => {
+    if(month <= 3 ){
+      return 1
+    }else if(month <= 6){
+      return 2
+    }else if(month <= 9){
+      return 3
+    }else if(month <=12){
+      return 4
+    }
+  }
+  
+  /* Q1 = 1-3, Q2 = 4-6, Q3 = 7-9, Q4 = 10-12 */ 
+  
+  
