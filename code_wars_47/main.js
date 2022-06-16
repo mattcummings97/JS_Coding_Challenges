f978ca32f26484b79411dedb05a438e20c8a6a75
Matthew Cummings <cummingsmatthew97@gmail.com>
2022-06-16 10:24:53 -0400

    new challenge

---
+++ code_wars_47/main.js
@@ -0,0 +1,14 @@
+/* You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not. */ 
+
+function check(a, x){
+    const checkFor = a.includes(x);
+    if(checkFor === true){
+      return true;
+    }else{
+      return false;
+    }
+  }
